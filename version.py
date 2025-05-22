93877dbcd1affec9264bb5b084ebf6ecdaa6a5f6
afrizal0811 <afrizalmaulana0811@gmail.com>
2025-05-22 14:47:27 +0700

    update v1.0.2

--- version.py
+++ version.py
@@ -1,3 +1,3 @@
-CURRENT_VERSION = "1.0.1"
+CURRENT_VERSION = "1.0.2"
 REMOTE_VERSION_URL = "https://raw.githubusercontent.com/afrizal0811/tms-app/refs/heads/main/version.txt"
 DOWNLOAD_LINK = "https://github.com/afrizal0811/tms-app/releases/latest"  # kalau kamu nanti taruh installer di GitHub Releases
